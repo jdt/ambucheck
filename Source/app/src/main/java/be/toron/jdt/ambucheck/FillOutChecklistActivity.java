9306083fd695319dc638a9a4e9769e5b19e79eca
Jesper De Temmerman <jesper@toron.be>
2015-09-10 20:32:48 +0200

    Added button on the main screen to fill out checklist and empty activity launched by it

--- Source/app/src/main/java/be/toron/jdt/ambucheck/FillOutChecklistActivity.java
+++ Source/app/src/main/java/be/toron/jdt/ambucheck/FillOutChecklistActivity.java
@@ -1,31 +1,24 @@
 package be.toron.jdt.ambucheck;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class FillOutChecklistActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void fillOutChecklist(View view) {
-        Intent intent = new Intent(this, FillOutChecklistActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_fill_out_checklist);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_fill_out_checklist, menu);
         return true;
     }
 
