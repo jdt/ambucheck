408e3dfe394adb89d0341fd210982fdcdc753514
Jesper De Temmerman <jesper@toron.be>
2015-12-20 14:45:20 +0100

    Added fields for completed on/completed by

--- Source/Android/app/src/main/java/be/toron/jdt/ambucheck/domain/CheckList.java
+++ Source/Android/app/src/main/java/be/toron/jdt/ambucheck/domain/CheckList.java
@@ -1,12 +1,19 @@
 package be.toron.jdt.ambucheck.domain;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class CheckList
 {
     private DefaultCheckListItemCollection _items;
+    private Date _completedOn;
+    private String _completedBy;
 
     public CheckList()
     {
         _items = new DefaultCheckListItemCollection();
+        _completedOn = Calendar.getInstance().getTime();
+        _completedBy = "";
     }
 
     public CheckListItemCollection getCheckListItems()
@@ -17,5 +24,25 @@ public class CheckList
     public void addCheckListItem(CheckListItem item)
     {
         _items.add(item);
+    }
+
+    public void setCompletedOn(Date on)
+    {
+        _completedOn = on;
+    }
+
+    public Date getCompletedOn()
+    {
+        return _completedOn;
+    }
+
+    public void setCompletedBy(String name)
+    {
+        _completedBy = name;
+    }
+
+    public String getCompletedBy()
+    {
+        return _completedBy;
     }
 }
